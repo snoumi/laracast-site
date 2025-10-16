174091ff1281c0de1f6d77440d5faa69718e34d1
snoumi <snoumimefl@gmail.com>
2025-10-16 10:50:27 +0200

    Container for notes, bootstrap, App, updated notes, controllers

--- public/index.php
+++ public/index.php
@@ -13,11 +13,12 @@ spl_autoload_register(function ($class) {
     require base_path("{$class}.php");
 });
 
+require base_path("bootstrap.php");
+
 $router = new Router();
-
 $routes = require base_path("routes.php");
-$uri = parse_url($_SERVER['REQUEST_URI'])["path"];
 
+$uri = parse_url($_SERVER['REQUEST_URI'])["path"];
 $method = $_POST["_method"] ?? $_SERVER['REQUEST_METHOD'];
 
 $router->route($uri, $method);
